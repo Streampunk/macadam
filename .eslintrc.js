414a8d072f2f6ee8f33aae3014f8ab18aaa7102d
Richard Cartwright <spark@streampunk.media>
2018-11-23 18:55:44 +0000

    Testing timcode, adding linter and tape.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "off",
+        "prefer-arrow-callback": "error"
+    }
+};
