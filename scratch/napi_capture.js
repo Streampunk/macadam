5da757dcd78fa54b006cb91f9d58a2ab4f63b8a4
Richard Cartwright <spark@streampunk.media>
2018-11-27 20:22:47 +0000

    Testing timecode with scratch files.

--- scratch/napi_capture.js
+++ scratch/napi_capture.js
@@ -2,8 +2,8 @@ const macadam = require('../index.js');
 
 async function run() {
   let capture = await macadam.capture({
-    pixelFormat: macadam.bmdFormat8BitYUV,
-    channels: 2
+    displayMode: macadam.bmdModeHD1080i2997,
+    pixelFormat: macadam.bmdFormat10BitYUV
   });
   console.log(capture);
   for ( let x = 0 ; x < 1000 ; x++ ) {
@@ -11,6 +11,7 @@ async function run() {
     /* let frame = await Promise.all([
       capture.frame(), capture.frame(), capture.frame()
     ]); */
+    console.log(x, frame.video.timecode);
     if (x % 100 === 0) console.log(x, frame);
     // frame = null;
     // if (x % 10 === 0) global.gc();
